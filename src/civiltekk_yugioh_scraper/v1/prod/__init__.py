a1e160728c103463a42be07511c08bae81d1aeac
darellchua <kkchua@betekk.com>
2025-09-20 23:04:43 +0800

    remove import not used

--- src/civiltekk_yugioh_scraper/v1/prod/__init__.py
+++ src/civiltekk_yugioh_scraper/v1/prod/__init__.py
@@ -1,5 +1,5 @@
 from .ygo_inventory_upload import upload_inventory_main
-from .ygo_inventory_export import export_inventory_excel, combine_ae_price, export_inventory_excel_v2
+from .ygo_inventory_export import combine_ae_price, export_inventory_excel_v2
 from .card_list_scraper import card_list_scraper
 from .bigwebscrape import bigweb_scrape
 from .yuyuteiscrape2 import yuyutei_scrape
